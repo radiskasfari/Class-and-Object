2588e68d5a73889428322c3ed6ddbc8080282598
radiskasfari <radiskasfari@students.amikom.ac.id>
2022-03-26 17:15:46 +0700

    Tugas Taxi

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_dan_Object_Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            Taxi taxi = new Class_dan_Object_Taxi.Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriveName = "Nabsyah";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 20;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_dan_Object_Taxi
+{
+    public class Taxi
+    {
+        //properties
+        public string DriveName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriveName);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriveName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriveName);
+        }
+    }
+}
